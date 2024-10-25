a4e9251b9b47221a39bfd2b3900dbede948e06bb
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-10-25 20:57:11 +0200

    change the package

--- lib/webview_maritime_zone/web_view_maritime_zone.dart
+++ lib/webview_maritime_zone/web_view_maritime_zone.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebViewMaritimeZone extends StatefulWidget {
   @override
@@ -7,43 +7,40 @@ class WebViewMaritimeZone extends StatefulWidget {
 }
 
 class _WebViewMaritimeZoneState extends State<WebViewMaritimeZone> {
-  late final WebViewController controller;
+  late InAppWebViewController controller;
   final String allowedUrl = 'https://maritime-zone.com/en/vacancy';
 
   @override
   void initState() {
     super.initState();
-
-    // Инициализация WebViewController
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {},
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onHttpError: (HttpResponseError error) {},
-          onWebResourceError: (WebResourceError error) {},
-          // Ограничиваем навигацию на другие URL
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith(allowedUrl)) {
-              return NavigationDecision
-                  .navigate; // Разрешаем только основной URL
-            } else {
-              return NavigationDecision
-                  .prevent; // Блокируем все остальные переходы
-            }
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(allowedUrl));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Maritime Zone')),
-      body: WebViewWidget(controller: controller),
+      body: InAppWebView(
+        initialUrlRequest:
+            URLRequest(url: WebUri(allowedUrl)), // Используем WebUri
+        onWebViewCreated: (InAppWebViewController webViewController) {
+          controller = webViewController;
+        },
+        onLoadStart: (InAppWebViewController webViewController, WebUri? url) {
+          // Можно использовать для отладки
+        },
+        onLoadStop:
+            (InAppWebViewController webViewController, WebUri? url) async {
+          // Можно использовать для обработки после загрузки
+        },
+        shouldOverrideUrlLoading: (InAppWebViewController webViewController,
+            NavigationAction request) async {
+          if (request.request.url!.toString().startsWith(allowedUrl)) {
+            return NavigationActionPolicy.ALLOW; // Разрешаем навигацию
+          } else {
+            return NavigationActionPolicy.CANCEL; // Блокируем навигацию
+          }
+        },
+      ),
     );
   }
 }
